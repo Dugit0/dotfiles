6afbf6b4b8086c8bf5b384ec968703bc082a21b5
Dugit0 <dmitry.astrakhantsev@gmail.com>
2024-07-25 03:24:45 +0300

    Add script for init vimrc and bashrc

---
+++ init_bash_vim_conf.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+touch "$HOME/.vimrc"
+echo 'source ~/.config/vim/vimrc' > "$HOME/.vimrc"
+cat "$HOME/.vimrc"
+touch "$HOME/.bashrc"
+echo 'source ~/.config/bash/bashrc' > "$HOME/.bashrc"
