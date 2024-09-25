7c180f959f4c71679f38f68f60a47601362b38bc
Dugit0 <dmitry.astrakhantsev@gmail.com>
2024-09-25 15:25:54 +0300

    Fix init bash and vim script

--- init_bash_vim_conf.bash
+++ init_bash_vim_conf.bash
@@ -1,7 +1,9 @@
 # #!/bin/bash
 touch "$HOME/.vimrc"
+cp "$HOME/.vimrc" "$HOME/.vimrc.bak"
 echo 'source ~/.config/vim/vimrc' > "$HOME/.vimrc"
 # cat "$HOME/.vimrc"
 touch "$HOME/.bashrc"
-echo 'source ~/.config/bash/bashrc' > "$HOME/.bashrc"
+cp "$HOME/.bashrc" "$HOME/.bashrc.bak"
+echo 'source ~/.config/bash/bashrc' >> "$HOME/.bashrc"
 # cat "$HOME/.bashrc"
