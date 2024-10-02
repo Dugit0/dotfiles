f917e8cdbf23056e56130b154a2a48abcaec92e4
Dugit0 <dmitry.astrakhantsev@gmail.com>
2024-10-02 15:27:41 +0300

    Change cpp standart to cpp23

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -33,7 +33,7 @@ function cmcgcc() {
 function cmcgpp() {
     outfile=${1%%.*}
     echo "${outfile}.out"
-    g++ -fsanitize=address,undefined,signed-integer-overflow,pointer-compare,pointer-subtract,leak,bounds,pointer-overflow -O2 -Wall -Wextra -std=c++20 -lm -o "${outfile}.out" ${1}
+    g++ -fsanitize=address,undefined,signed-integer-overflow,pointer-compare,pointer-subtract,leak,bounds,pointer-overflow -O2 -Wall -Wextra -std=c++23 -lm -o "${outfile}.out" ${1}
 }
 # -Werror
 
